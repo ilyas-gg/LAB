999ba9e94bfa1473a81e81de8f7b90748cb9a738
ilyas-gg <ilyakoval2006@gmail.com>
2024-12-19 16:51:37 +0300

    Delete block_four1.py

--- block_four1.py
+++
@@ -1,8 +0,0 @@
-numbers=[1,2,3,4,5,6,7,8,9]
-oddnumbers=[num for num in numbers if num %2 !=0]
-if oddnumbers:
-    min_odd=min(oddnumbers)
-    print("Наименьший нечетный элемент")
-else:
-    print("В списке нету нечетных")
-    
